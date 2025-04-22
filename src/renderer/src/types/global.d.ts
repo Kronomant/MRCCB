532436542ef040b12a9a81c602940adc78c830cb
Gabriel Martins Xavier <gabrim0070@gmail.com>
2025-04-21 23:41:11 -0300

    feat: create mock data
    Criei o arquivo com o mock de dados, tentei fazer de forma a parecer mais com a chamada de funções de service

---
+++ src/renderer/src/types/global.d.ts
@@ -0,0 +1,51 @@
+declare global {
+  interface Reunion {
+    id: number
+    name: string  
+    value: number
+    treatmentQuantity: number
+    foodBasketQuantity: number
+    date: String
+  }
+
+  interface Unity {
+    id: number
+    name: string
+  }
+
+  interface Enchiridion {
+    id: number
+    number: number
+    unityId: number
+    ministry: boolean
+  }
+
+  interface Treatment {
+    id: number
+    enchiridionId: number
+    reunionId: number
+    unityId: number
+    date: String
+    aprovedValue: boolean
+    value: number
+    foodBasketQuantity: number
+    onlyClothes: boolean
+    emergency: boolean
+    returned: boolean
+    repeat: boolean
+  }
+
+  interface FoodBasket {
+    id: number
+    date: String
+    exit: boolean
+  }
+
+  interface DropboxBackup {
+    id: number
+    dropboxAccount: string
+    backupPath: string
+  }
+}
+
+export {};
